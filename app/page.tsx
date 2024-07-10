5033c3e7db0ee82db9daf4ef70376ec94b474a6a
realzegroeg <91103245+realzegroeg@users.noreply.github.com>
2024-07-10 14:21:28 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white">
       <head>
         <title>ACoP 2024</title>
       </head>
+      <h1>ACoP's AWESOME TOOLBOX TO SUCCESS</h1>
       <Image
         src="/graphic.svg"
         alt="Tool Box"
