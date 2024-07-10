6e9d4d5656622cbe836075019ed852e1e9372431
realzegroeg <91103245+realzegroeg@users.noreply.github.com>
2024-07-10 14:38:26 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white">
-      <h1>THE ACoP TOOLKIT TO SUCCESS</h1>
       <Image
         src="/graphic.svg"
         alt="Tool Box"
