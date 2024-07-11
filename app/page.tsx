05c2b0c06d275af0f94efce68ce288528f0ebd71
realzegroeg <91103245+realzegroeg@users.noreply.github.com>
2024-07-11 11:57:03 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const ToolBox = () => {
   return (
-    <svg width="200" height="100">
-      <rect x="10" y="10" width="50" height="50" fill="blue" />
-      <circle cx="100" cy="50" r="25" fill="red" />
-    </svg>
+    <div className="h-screen overflow-hidden>
+      <svg width="100%" height="100%">
+
+        <rect x="10" y="10" width="50" height="50" fill="blue" />
+        <circle cx="100" cy="50" r="25" fill="red" />
+      
+      </svg>
+    </div>
   );
 };
 
