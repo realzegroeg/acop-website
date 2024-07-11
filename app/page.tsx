6db848f6fb47aa1c3d6bc3e925d972502a311243
realzegroeg <91103245+realzegroeg@users.noreply.github.com>
2024-07-11 15:27:30 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const ToolBox = () => {
         width="800" 
         height="450" 
         src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQVSMCgC3XusaNBWDClp6uT%2FAPoC-Website-Design%3Fnode-id%3D1-2%26t%3DWGxEqpfQUSGtVAnx-1" 
-        allowfullscreen
+        allowFullScreen={true} // Correct property name
       ></iframe>
     </div>
   );
