5fc7fe7dafbf133d553e5651bd84423eb19068b6
realzegroeg <91103245+realzegroeg@users.noreply.github.com>
2024-07-11 11:48:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
-
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen flex flex-col items-center justify-between">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        {/* Your main content goes here */}
+      </main>
+      <div className="w-full h-20 bg-gray-200 rounded-t-lg flex items-center justify-center">
+        <div className="w-48 h-12 bg-blue-500 rounded-lg"></div> {/* Example shape */}
+      </div>
+    </div>
+  );
+}
