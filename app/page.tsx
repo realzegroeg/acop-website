c4f29298592e24e0165aa89f1ae7fdb7b7259ce4
realzegroeg <91103245+realzegroeg@users.noreply.github.com>
2024-07-11 15:26:21 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const ToolBox = () => {
   return (
     <div className="h-screen overflow-hidden bg-white">
       <iframe 
-        style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+        className="figma-iframe" 
         width="800" 
         height="450" 
         src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQVSMCgC3XusaNBWDClp6uT%2FAPoC-Website-Design%3Fnode-id%3D1-2%26t%3DWGxEqpfQUSGtVAnx-1" 
